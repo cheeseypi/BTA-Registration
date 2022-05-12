e77c9a19883d4fc5a665ba6460f03c5492aa7c6c
Skye Doto <skye@doto.works>
2022-05-11 21:55:17 -0400

    Update to get place

--- API/index.js
+++ API/index.js
@@ -32,51 +32,53 @@ API REFERENCE
   Retrieves all BTA participants with their preliminary placements
 */
 
-function getPB(runList){
-    let pb = -1
-    for(let run of runList){
-        if(pb < 0 || run.times.primary_t < pb){
-            pb = run.times.primary_t;
-        }
-    }
-    return pb;
-}
-function pprintSeconds(totalSeconds){
-    let minutes = Math.floor(totalSeconds / 60);
+function pprintSeconds(totalSeconds) {
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor(totalSeconds / 60) - (hours*60);
     let seconds = Math.floor(totalSeconds % 60);
     let milliseconds = Math.round((totalSeconds - Math.floor(totalSeconds)) * 1000);
-    return minutes+"m"+seconds+"s"+milliseconds+"ms";
+    return (hours>0 ? hours + "h" : "") + minutes + "m" + seconds + "s" + milliseconds + "ms";
 }
 
 app.get('/PBs/:userId', async (req, res) => {
     let srcUserName = req.params.userId;
-    let userList = await got.get("https://www.speedrun.com/api/v1/users?name="+srcUserName).json();
+    let userList = await got.get("https://www.speedrun.com/api/v1/users?name=" + srcUserName).json();
     let users = userList.data.filter(x => x.names.international.toLowerCase() === srcUserName.toLowerCase());
-    if(users.length > 0) {
+    if (users.length > 0) {
         let user = users[0];
         let userId = user.id;
-        let runs = await got.get("https://www.speedrun.com/api/v1/runs?user="+userId+"&game=o1y9j9v6&status=verified").json();
+        let runs = await got.get("https://www.speedrun.com/api/v1/users/" + userId + "/personal-bests?game=o1y9j9v6").json();
         let runList = runs.data;
-        while(runs.pagination.links.some(x => x.rel === "next")){
+        while (runs.pagination?.links?.some(x => x.rel === "next")) {
             let nextLink = runs.pagination.links.filter(x => x.rel === "next")[0]
             runs = await got.get(nextLink.uri).json();
             runList.push(...runs.data);
         }
         let finalRet = {}
-        for(let cat of categories.categories){
-            let pb = getPB(runList.filter(x => x.category === cat.id));
-            finalRet[cat.id] = {"name": cat.name, "time": pb, "humanReadable": pprintSeconds(pb)};
-        }
-        for(let level of categories.levels){
-            finalRet[level.id] = {"name": level.name}
-            for(let cat of level.categories){
-                let pb = getPB(runList.filter(x => x.level === level.id && x.category === cat.id));
-                finalRet[level.id][cat.id] = {"name": cat.name, "time": pb, "humanReadable": pprintSeconds(pb)};
+        for (let run of runList) {
+            let place = run.place;
+            let runObj = run.run;
+            let level = runObj.level;
+            let category = runObj.category;
+            let status = runObj.status.status;
+            let time = runObj.times.primary_t;
+            if (status === 'verified') {
+                if (level) {
+                    let levelName = categories.levels[level]?.name;
+                    let name = categories.levels[level]?.categories?.[category]?.name;
+                    finalRet[level] = finalRet[level] || {};
+                    finalRet[level]["name"] = levelName;
+                    finalRet[level][category] = { "name": name, "time": time, "humanReadable": pprintSeconds(time), "place": place }
+                }
+                else {
+                    let name = categories.categories[category]?.name;
+                    finalRet[category] = { "name": name, "time": time, "humanReadable": pprintSeconds(time), "place": place }
+                }
             }
         }
         res.json(finalRet)
     }
-    else{
+    else {
         res.statusCode(404).send("Unable to find user in SRC, please make sure you haven't entered a typo");
     }
 })
