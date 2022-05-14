2604514e89ae60bfa1743e03d1912a4d2f3c6d5b
Skye Doto <skye@doto.works>
2022-05-14 19:20:50 -0400

    Update client with material UI and router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <Typography variant="body1" component="p">Home</Typography>
+    </>
+  )
+}
+function Reg() {
+  return (
+    <>
+      <Typography variant="body1" component="p">Reg</Typography>
+    </>
+  )
+}
+function Board() {
+  return (
+    <>
+      <Typography variant="body1" component="p">Board</Typography>
+    </>
+  )
+}
+function Header() {
+  let navigate = useNavigate()
+  return(
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="header" sx={{ flexGrow: 1 }}>
+              BTA Registration
+            </Typography>
+            <Button color="inherit" onClick={()=>navigate('/')}>About</Button>
+            <Button color="inherit" onClick={() => navigate('/register')}>
+              Register
+            </Button>
+            <Button color="inherit" onClick={() => navigate('/board')}>
+              Board
+            </Button>
+          </Toolbar>
+        </AppBar>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Box sx={{ flexGrow: 1 }}>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Reg />}>
+          </Route>
+          <Route path="/board" element={<Board />}>
+          </Route>
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
